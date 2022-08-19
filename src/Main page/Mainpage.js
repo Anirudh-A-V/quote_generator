522ea0f149471d5ac8f9cb6731eff93db11030ad
Anirudh A V <anirudh.av02@gmail.com>
2022-08-19 12:25:37 +0530

    All files created

---
+++ src/Main page/Mainpage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Mainpage.css'
+
+const Mainpage = () => {
+    return (
+        <></>
+    )
+}
+
+export default Mainpage;
---
+++ src/components/Bookmarks.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef, useState } from "react";
+import './Bookmarks.css';
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { addBookmark, deleteBookmark, getBookmarks } from "../../actions/bookmarkActions";
+// import { useHistory } from "react-router-dom";
+// import { useForm } from "react-hook-form";
+// import { useTranslation } from "react-i18next";
+
+const Bookmarks = () => {
+    return (
+        <></>
+    );
+}
+
+export default Bookmarks;
